d9499db4aa8539b57f40f1f67c25208ade336b90
unsupo <unsupo@gmail.com>
2016-09-23 16:04:28 -0600

    started adding missionbuilder
    Former-commit-id: 18126b23f19ef0bede06010c01e729c51afe6540

--- src/main/java/utilities/ogame/MissionBuilder.java
+++ src/main/java/utilities/ogame/MissionBuilder.java
@@ -1,7 +1,9 @@
 package utilities.ogame;
 
 import objects.Coordinates;
+import objects.Fleet;
 import ogame.utility.Initialize;
+import ogame.utility.Resource;
 import utilities.selenium.UIMethods;
 
 import java.io.IOException;
@@ -22,6 +24,93 @@ public class MissionBuilder {
                                 ACS_ATTACK              = "missionButton2",
                                 MOON_DESTRUCTION        = "missionButton9";
 
+    private Fleet fleet;
+    private String mission;
+    private int speed;
+
+    @Override
+    public String toString() {
+        return "MissionBuilder{" +
+                " mission='" + mission + '\'' +
+                ", speed=" + speed +
+                ", destination=" + destination +
+                ", resourceToSend=" + resourceToSend +
+                ", fleet=" + fleet +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MissionBuilder that = (MissionBuilder) o;
+
+        if (speed != that.speed) return false;
+        if (fleet != null ? !fleet.equals(that.fleet) : that.fleet != null) return false;
+        if (mission != null ? !mission.equals(that.mission) : that.mission != null) return false;
+        if (destination != null ? !destination.equals(that.destination) : that.destination != null) return false;
+        return resourceToSend != null ? resourceToSend.equals(that.resourceToSend) : that.resourceToSend == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = fleet != null ? fleet.hashCode() : 0;
+        result = 31 * result + (mission != null ? mission.hashCode() : 0);
+        result = 31 * result + speed;
+        result = 31 * result + (destination != null ? destination.hashCode() : 0);
+        result = 31 * result + (resourceToSend != null ? resourceToSend.hashCode() : 0);
+        return result;
+    }
+
+    Coordinates destination;
+    Resource resourceToSend;
+
+    public Fleet getFleet() {
+        return fleet;
+    }
+
+    public MissionBuilder setFleet(Fleet fleet) {
+        this.fleet = fleet;
+        return this;
+    }
+
+    public String getMission() {
+        return mission;
+    }
+
+    public MissionBuilder setMission(String mission) {
+        this.mission = mission;
+        return this;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public MissionBuilder setSpeed(int speed) {
+        this.speed = speed;
+        return this;
+    }
+
+    public Coordinates getDestination() {
+        return destination;
+    }
+
+    public MissionBuilder setDestination(Coordinates destination) {
+        this.destination = destination;
+        return this;
+    }
+
+    public Resource getResourceToSend() {
+        return resourceToSend;
+    }
+
+    public MissionBuilder setResourceToSend(Resource resourceToSend) {
+        this.resourceToSend = resourceToSend;
+        return this;
+    }
 
     public static void attackTarget(String attackingFromPlanetName, Coordinates yourTargetsCoordinates) throws IOException {
         UIMethods.clickOnAttributeAndValue("id", Initialize.getPlanetMap().get(attackingFromPlanetName).getWebElement());
