9d6825f71cf9d4249da29904708cd6c6d7e4a9cf
jarndt <JemmyJemmy82@>
2017-05-01 16:40:35 -0600

    Getting Started Commit
    Former-commit-id: 618e659a3e0f79bf156adcca4102e4452ad3fd6d

---
+++ ogamebotserver/src/main/java/application/Application.java
@@ -0,0 +1,14 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jarndt on 4/13/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ ogamebotserver/src/main/resources/static/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('app',
+    [
+        'ngMaterial',
+        'ui.router',
+        'ngMdIcons',
+        'ngCookies',
+        'ui.ace'
+    ])
+    .config(['$mdThemingProvider', function($mdThemingProvider) {
+        $mdThemingProvider.theme('input', 'default')
+            .primaryPalette('grey')
+
+        // var lightGreenMap = $mdThemingProvider.extendPalette('light-green', {
+        //     '200': 'aecc5a'
+        // });
+        // $mdThemingProvider.definePalette('lighter-green', lightGreenMap);
+        // $mdThemingProvider.theme('default')
+        //     .primaryPalette('light-green')
+        //     .accentPalette('lighter-green', {
+        //         'default': '200'
+        //     })
+    }]);
+
