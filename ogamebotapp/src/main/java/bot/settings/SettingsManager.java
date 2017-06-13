3899d5033f307bcfd7e4a2970526c34522d0f4d1
jarndt <JemmyJemmy82@>
2017-06-13 16:53:52 -0600

    Added settings manager
    Former-commit-id: d5461ed9b24edc5f2180f8f4bef7cb74a729859f

--- ogamebotapp/src/main/java/bot/settings/SettingsManager.java
+++ ogamebotapp/src/main/java/bot/settings/SettingsManager.java
@@ -1,7 +1,86 @@
 package bot.settings;
 
+import ogame.objects.game.planet.Planet;
+import utilities.database.Database;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * Created by jarndt on 6/9/17.
  */
 public class SettingsManager {
+    public static final String
+            DELETE_MESSAGES                 = "DELETE_MESSAGES".toLowerCase(),
+            AUTO_BUILD_METAL_STORAGE        = "AUTO_BUILD_METAL_STORAGE".toLowerCase(),
+            AUTO_BUILD_CRYSTAL_STORAGE      = "AUTO_BUILD_CRYSTAL_STORAGE".toLowerCase(),
+            AUTO_BUILD_DEUTERIUM_STORAGE    = "AUTO_BUILD_DEUTERIUM_STORAGE".toLowerCase(),
+            AUTO_BUILD_SOLAR                = "AUTO_BUILD_SOLAR".toLowerCase(),
+            AUTO_BUILD_SOLAR_PERCENT        = "AUTO_BUILD_SOLAR_PERCENT".toLowerCase(),
+            SIMULATE_QUEUE_ON_EMPTY         = "SIMULATE_QUEUE_ON_EMPTY".toLowerCase();
+
+    public static final List<String> SETTINGS = Arrays.asList(
+            DELETE_MESSAGES, AUTO_BUILD_CRYSTAL_STORAGE,AUTO_BUILD_DEUTERIUM_STORAGE,AUTO_BUILD_METAL_STORAGE,
+            AUTO_BUILD_SOLAR,AUTO_BUILD_SOLAR_PERCENT,SIMULATE_QUEUE_ON_EMPTY
+    );
+
+
+    private transient Database d;
+    private Database getDatabase() throws SQLException, IOException, ClassNotFoundException {
+        if(d == null)
+            d = Database.newDatabaseConnection();
+        return d;
+    }
+
+    private HashMap<String,String> settings;
+    private transient Planet planet;
+    private String planetId;
+
+    public SettingsManager(Planet planet) {
+        this.planet = planet;
+        this.planetId = planet.getBotPlanetID();
+    }
+
+    public HashMap<String, String> getSettings() throws SQLException {
+        if(settings == null)
+            settings = new HashMap<>();
+        List<Map<String, Object>> v = d.executeQuery("select * from config where bot_planets_id = " + planetId);
+        if(v != null && v.size() > 0 && v.get(0) != null && v.get(0).size() > 0)
+            v.get(0).forEach((a, b)->settings.put(a,b!=null?b.toString():null));
+        fillInDefaults();
+        return settings;
+    }
+
+    private void fillInDefaults() {
+        HashMap<String,String> defaults = new HashMap<>();
+        defaults.put(DELETE_MESSAGES,"true");
+        defaults.put(AUTO_BUILD_METAL_STORAGE,"false");
+        defaults.put(AUTO_BUILD_CRYSTAL_STORAGE,"false");
+        defaults.put(AUTO_BUILD_DEUTERIUM_STORAGE,"false");
+        defaults.put(AUTO_BUILD_SOLAR,"false");
+        defaults.put(AUTO_BUILD_SOLAR_PERCENT,"80");
+        defaults.put(SIMULATE_QUEUE_ON_EMPTY,"false");
+
+        SETTINGS.forEach(a->{
+            if(!(settings.containsKey(a) && settings.get(a) != null))
+                settings.put(a,defaults.get(a));
+        });
+    }
+
+    public void setSettings(HashMap<String, String> settings) {
+        this.settings = settings;
+    }
+
+    public String getPlanetId() {
+        return planetId;
+    }
+
+    public void setPlanetId(String planetId) {
+        this.planetId = planetId;
+    }
 }
